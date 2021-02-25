9adbf062cbb0d7ba448292807d88ac1e4cee77b4
Gleb <gleb.kosheev@yandex.ru>
2021-02-24 19:47:11 -0500

    changed server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/Web-App'));
+app.use(express.static('../Web-App'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/Web-App/'}),
+  res.sendFile('index.html', {root: '../Web-App/src'}),
 );
 
 app.listen(process.env.PORT || 8080);
