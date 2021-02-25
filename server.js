ac1f72c2bbeb3d93d70748a0cd1a78df912eb071
Gleb <gleb.kosheev@yandex.ru>
2021-02-24 19:07:12 -0500

    fixed server.js for heroku deploy

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('../Web-App/src'));
+app.use(express.static('./dist/Web-App'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '../Web-App/src'}),
+  res.sendFile('index.html', {root: 'dist/Web-App/'}),
 );
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
