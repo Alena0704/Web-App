63344781489d4e4d266a7b7c4104249055d369b7
Gleb <gleb.kosheev@yandex.ru>
2021-02-25 17:00:30 -0500

    this might be the last one

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('../dist/al'));
+app.use(express.static(__dirname + '/dist/al'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join('/dist/al/index.html'));
+res.sendFile(path.join(__dirname+'/dist/al/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
