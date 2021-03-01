e1d1255e0e11f1281dfa204fae6cc3b00c5e89a5
Gleb Kosheev <gleb.kosheev@yandex.ru>
2021-03-01 15:43:31 -0800

    deleted useless comments

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-
-/*
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('../Web-App'));
-
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: '../Web-App/src'}),
-);
-
-app.listen(process.env.PORT || 8080);
-*/
-
-
-//______________________________
-
 //Install express server
 const express = require('express');
 const path = require('path');
