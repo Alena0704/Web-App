330cbf61be381fdb68ba1ccfb18e1cce74f84528
Gleb <gleb.kosheev@yandex.ru>
2021-03-01 22:48:25 -0500

    added pagination and filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-import {DemoMaterialModule} from '../material-module';
 
 export interface ProjectData {
   id: number;
@@ -15,13 +15,86 @@ export interface ProjectData {
 }
 
 const ELEMENT_DATA: ProjectData[] = [
-  {id: 1, workTitle: 'Я', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '1Pizdec', source_: 'source', recommends: '3'},
-  {id: 2, workTitle: 'СЕЙЧАС', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '2Pizdec', source_: 'source', recommends: '4'},
-  {id: 3, workTitle: 'ВЫПИЛЮСЬ', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '3Pizdec', source_: 'source', recommends: '5'},
-  {id: 4, workTitle: 'А', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '4Pizdec', source_: 'source', recommends: '2'},
-  {id: 5, workTitle: 'НЕТ', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '5Pizdec', source_: 'source', recommends: '1'},
-  {id: 6, workTitle: 'Я', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '6Pizdec', source_: 'source', recommends: '0'},
-  {id: 7, workTitle: 'ВСКРОЮСЬ', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '7Pizdec', source_: 'source', recommends: '6'}
+  {
+    id: 1,
+    workTitle: 'Я',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '1Pizdec',
+    source_: 'source',
+    recommends: '3'
+  },
+  {
+    id: 2,
+    workTitle: 'СЕЙЧАС',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '2Pizdec',
+    source_: 'source',
+    recommends: '4'
+  },
+  {
+    id: 3,
+    workTitle: 'ВЫПИЛЮСЬ',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '3Pizdec',
+    source_: 'source',
+    recommends: '5'
+  },
+  {
+    id: 4,
+    workTitle: 'А',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '4Pizdec',
+    source_: 'source',
+    recommends: '2'
+  },
+  {
+    id: 5,
+    workTitle: 'НЕТ',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '5Pizdec',
+    source_: 'source',
+    recommends: '1'
+  },
+  {
+    id: 6,
+    workTitle: 'Я',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '6Pizdec',
+    source_: 'source',
+    recommends: '0'
+  },
+  {
+    id: 7,
+    workTitle: 'ВСКРОЮСЬ',
+    endDate: 'EPTODAY',
+    classType: 'lecture',
+    studyLink: 'https://achika.herokuapp.com/',
+    result: '7Pizdec',
+    source_: 'source',
+    recommends: '6'
+  },
+  {
+    id: 8,
+    workTitle: 'Чикадрило',
+    endDate: 'Тупой',
+    classType: 'Бездарный',
+    studyLink: 'Хуесос',
+    result: 'И',
+    source_: 'Гнилой',
+    recommends: 'Аутист'
+  }
 ];
 
 
@@ -32,16 +105,31 @@ const ELEMENT_DATA: ProjectData[] = [
 })
 export class HomeComponent implements AfterViewInit {
   constructor() {
+
   }
 
   displayedColumns: string[] = ['id', 'workTitle', 'endDate', 'classType', 'studyLink', 'result', 'source_', 'recommends'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator: MatPaginator | any;
   @ViewChild(MatSort) sort: MatSort = new MatSort();
 
+
   ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
 
 
