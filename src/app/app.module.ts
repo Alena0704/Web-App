86e810e7acc8f8969b2ecac37c8f86dae4d6ffc8
Gleb <gleb.kosheev@yandex.ru>
2021-03-01 21:09:43 -0500

    added angular component table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {RouterModule} from "@angular/router";
-
-import {AppRoutingModule} from './app-routing.module';
+import {RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {ProfileComponent} from './profile/profile.component';
 import {LoginComponent} from './login/login.component';
 import {AdminSubjectsComponent} from './admin/admin-subjects/admin-subjects.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {HomeComponent} from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {DemoMaterialModule} from './material-module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -21,19 +26,26 @@ import {HomeComponent} from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
       {path: 'login', component: LoginComponent},
       {path: 'profile', component: ProfileComponent},
       {path: 'navbar', component: NavbarComponent},
       {path: 'admin-subjects', component: AdminSubjectsComponent}
-    ])
+    ]),
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }, ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
 }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,65 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {DemoMaterialModule} from '../material-module';
 
-export interface Suk {
-  "ID_работы": string,
-  "Название работы": string,
-  "Дата сдачи": string,
-  "Тип": string,
-  "Просмотр": string,
-  "Результат": string,
-  "Источники": string,
-  "Рекомендации": string
+export interface ProjectData {
+  id: number;
+  workTitle: string;
+  endDate: string;
+  classType: string;
+  studyLink: string;
+  result: string;
+  source_: string;
+  recommends: string;
 }
+
+const ELEMENT_DATA: ProjectData[] = [
+  {id: 1, workTitle: 'Я', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '1Pizdec', source_: 'source', recommends: '3'},
+  {id: 2, workTitle: 'СЕЙЧАС', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '2Pizdec', source_: 'source', recommends: '4'},
+  {id: 3, workTitle: 'ВЫПИЛЮСЬ', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '3Pizdec', source_: 'source', recommends: '5'},
+  {id: 4, workTitle: 'А', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '4Pizdec', source_: 'source', recommends: '2'},
+  {id: 5, workTitle: 'НЕТ', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '5Pizdec', source_: 'source', recommends: '1'},
+  {id: 6, workTitle: 'Я', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '6Pizdec', source_: 'source', recommends: '0'},
+  {id: 7, workTitle: 'ВСКРОЮСЬ', endDate: 'EPTODAY', classType: 'lecture', studyLink: 'https://achika.herokuapp.com/', result: '7Pizdec', source_: 'source', recommends: '6'}
+];
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements AfterViewInit {
   constructor() {
   }
 
-  testCol = ["c1", "c2", "c3"]
-  testRow: any = ["r1", "r2", "r3"]
+  displayedColumns: string[] = ['id', 'workTitle', 'endDate', 'classType', 'studyLink', 'result', 'source_', 'recommends'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
 
-  columns = [
-    "ID_работы",
-    "Название работы",
-    "Дата сдачи",
-    "Тип",
-    "Просмотр",
-    "Результат",
-    "Источники",
-    "Рекомендации"
-  ]
-
-  //skill, understanding, knowledge (suk)
-  suk: any = [{
-    "ID_работы": 'айди',
-    "Название работы": 'задание',
-    "Дата сдачи": 'дата окончения',
-    "Тип": 'тип занятия',
-    "Просмотр": 'ссылка на изучение',
-    "Результат": 'результат',
-    "Источники": 'источники',
-    "Рекомендации": 'рекомендации'
-  },
-    {
-      "ID_работы": 'айди',
-      "Название работы": 'задание',
-      "Дата сдачи": 'дата окончения',
-      "Тип": 'тип занятия',
-      "Просмотр": 'ссылка на изучение',
-      "Результат": 'результат',
-      "Источники": 'источники',
-      "Рекомендации": 'рекомендации'
-    }]
-
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
   }
-
 }
+
+
