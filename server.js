1db1cbf5e2b6eafd64793e83cc6de23adcb77244
Gleb Kosheev <gleb.kosheev@yandex.ru>
2021-03-02 22:02:56 -0800

    Update server.js
    commented database connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 
+/*
 const mysql = require('mysql');
 
 const dbConnect = mysql.createConnection({
@@ -19,14 +20,14 @@ dbConnect.connect(function(err){
 if (err) throw err;
 console.log("Database connected!!!!!");
 });
-
+*/
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/al'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function(req, res) {
 
-res.sendFile(path.join(__dirname+'/dist/al/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/al/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
