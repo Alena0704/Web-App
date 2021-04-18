488bc04590b2fda714584d408782c8ed7b30820a
Gleb <gleb.kosheev@yandex.ru>
2021-04-18 18:20:03 -0400

    added user interface

---
+++ src/app/shared/Interfaces/i-user.ts
@@ -0,0 +1,15 @@
+export interface IUser {
+  name: string;
+  surname: string;
+  patronymic: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  password: string;
+  // social networks
+  website: string;
+  github: string;
+  twitter: string;
+  instagram: string;
+  facebook: string;
+}
