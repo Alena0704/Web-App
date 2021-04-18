97ccc11e9bc1a48c01e24dc8256cdd5a2c3186a8
Gleb <gleb.kosheev@yandex.ru>
2021-04-18 12:56:15 -0400

    changed index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const app = require('./config/express')
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || port);
+// app.listen(process.env.PORT || port);
+// console.log("heroku started at: " + port);
+
+app.listen(port, function(){
+  console.log("Heroku started on port %d in %s mode", this.address().port, app.settings.env);
+})
